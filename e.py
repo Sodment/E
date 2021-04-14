6faad0c90b601b6b7d454c7a91004e62ad83e29a
Sodment <sodment@gmail.com>
2021-04-14 12:42:09 +0200

    E

---
+++ e.py
@@ -0,0 +1,24 @@
+def print_line_of_e(length):
+    for i in range(length):
+        print("E", end='')
+    print()
+
+
+def e_e(height, width):
+    line_len = height + width
+    second_point = (height - (2*width)) // 2
+    last_point = height - width
+    for e in range(width):
+        print_line_of_e(line_len)
+    for e in range(last_point - width):
+        if e == second_point:
+            for ee in range(width):
+                print_line_of_e(line_len - width)
+        else:
+            print_line_of_e(width)
+    for e in range(width):
+        print_line_of_e(line_len)
+    print()
+
+
+e_e(100, 12)
